4cdbb2293be9a03830334541464da84f1b35d402
Willis Chen <misweyu2007@gmail.com>
2017-01-19 12:16:04 +0800

    Edit calendar css for Django Oscar.

--- daterangepicker.js
+++ daterangepicker.js
@@ -100,7 +100,7 @@
                 '<div class="calendar left">' +
                     '<div class="daterangepicker_input">' +
                       '<input class="input-mini form-control" type="text" name="daterangepicker_start" value="" />' +
-                      '<i class="fa fa-calendar glyphicon glyphicon-calendar"></i>' +
+                      '<i class="fa fa-calendar glyphicon glyphicon-calendar icon-calendar glyphicon-calendar"></i>' +
                       '<div class="calendar-time">' +
                         '<div></div>' +
                         '<i class="fa fa-clock-o glyphicon glyphicon-time"></i>' +
@@ -111,7 +111,7 @@
                 '<div class="calendar right">' +
                     '<div class="daterangepicker_input">' +
                       '<input class="input-mini form-control" type="text" name="daterangepicker_end" value="" />' +
-                      '<i class="fa fa-calendar glyphicon glyphicon-calendar"></i>' +
+                      '<i class="fa fa-calendar glyphicon glyphicon-calendar icon-calendar glyphicon-calendar"></i>' +
                       '<div class="calendar-time">' +
                         '<div></div>' +
                         '<i class="fa fa-clock-o glyphicon glyphicon-time"></i>' +
